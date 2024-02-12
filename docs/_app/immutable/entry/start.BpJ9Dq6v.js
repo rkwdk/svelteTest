a694c473a8be843a710f0a7eb5f9c10d3f4e958d
zio <rkwdk2020@gmail.com>
2024-02-12 15:40:14 +0900

    add docs

---
+++ docs/_app/immutable/entry/start.BpJ9Dq6v.js
@@ -0,0 +1,1 @@
+import{b as a}from"../chunks/entry.CK7JEw6k.js";export{a as start};
--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,8 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	kit: {
 		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs',
 			fallback: '404.html'
 		}),
 		paths: {
